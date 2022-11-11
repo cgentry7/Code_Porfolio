6fc57a39be75acb7489b9ecb6926f845bcc8fe9b
Cole Gentry <cgentry7@gmail.com>
2022-11-11 16:01:02 -0500

    Improved analysis model

--- neural_network_polynomial_learner/analyses/quadratic_nn.py
+++ neural_network_polynomial_learner/analyses/quadratic_nn.py
@@ -19,11 +19,11 @@ poly        = Polynomial([2, 1, 1])
 test_points = full_domain_1000
 reference_y = [poly.f(x) for x in test_points[:,0]]
 
-hidden_layers_num_nodes = [16]
-dropouts                = [1.]
+hidden_layers_num_nodes = [64]
+dropouts = [1.0]
 
 # Only using "labeled" data to train with
-training_points = [half_domain_100] #0-th order derivative 
+training_points = [half_domain_100] #0-th order derivative
 d0_trained_data_nn = DerivativeInformedNN(poly, training_points, full_domain, hidden_layers_num_nodes, dropouts)
 d0_trained_data_nn.train(convergence_criteria=1E-2)
 d0_trained_data_y = d0_trained_data_nn.F(test_points)
@@ -40,10 +40,10 @@ d2_trained_data_nn = DerivativeInformedNN(poly, training_points, full_domain, hi
 d2_trained_data_nn.train(convergence_criteria=1E-2)
 d2_trained_data_y = d2_trained_data_nn.F(test_points)
 
-plt.plot(test_points[:,0],  reference_y,                            label="ref")
+plt.plot(test_points[:,0],  reference_y,            linestyle=':',  label="ref")
 plt.plot(test_points[:,0],  d0_trained_data_y[:,0], linestyle='-',  label="f_100")
-plt.plot(test_points[:,0],  d1_trained_data_y[:,0], linestyle='--', label="f_1_df_100")
-plt.plot(test_points[:,0],  d2_trained_data_y[:,0], linestyle='-.', label="f_1_df_1_d2f_100")
+plt.plot(test_points[:,0],  d1_trained_data_y[:,0], linestyle='-.', label="f_1_df_100")
+plt.plot(test_points[:,0],  d2_trained_data_y[:,0], linestyle='--', label="f_1_df_1_d2f_100")
 
 plt.legend()
 plt.show()
